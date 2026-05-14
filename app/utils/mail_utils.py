ac8c074f5fe1a24195c2de2b2a8b8f54c33d4732
DevaKalyan123 <devakalyaneepi@gmail.com>
2026-05-14 15:59:49 +0530

    Added the amail9

--- app/utils/mail_utils.py
+++ app/utils/mail_utils.py
@@ -1,43 +1,54 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 def send_otp_email(to_email, otp):
 
-    subject = "Your OTP Code"
+    sender_email = "devakalyaneepi@gmail.com"
+
+    app_password = "qwhxwmwflunoslwn"
+
+    subject = "AP RERA Admin OTP"
 
     body = f"""
+Hello Admin,
+
 Your OTP is: {otp}
 
-Do not share this OTP.
+Do not share this OTP with anyone.
+
+Regards,
+AP RERA Team
 """
 
-    msg = MIMEText(body)
+    msg = MIMEMultipart()
 
-    msg["Subject"] = subject
-    msg["From"] = "devakalyaneepi@gmail.com"
+    msg["From"] = sender_email
     msg["To"] = to_email
+    msg["Subject"] = subject
 
-    # Gmail SMTP
-    server = smtplib.SMTP("smtp.gmail.com", 587)
+    msg.attach(MIMEText(body, "plain"))
 
-    # Start TLS Security
-    server.starttls()
+    try:
 
-    # Login using Gmail + App Password
-    server.login(
-        "devakalyaneepi@gmail.com",
-        "qwhxwmwflunoslwn"
-    )
+        server = smtplib.SMTP("smtp.gmail.com", 587)
 
-    # Send Mail
-    server.sendmail(
-        "devakalyaneepi@gmail.com",
-        to_email,
-        msg.as_string()
-    )
+        server.starttls()
 
-    # Close Connection
-    server.quit()
+        server.login(sender_email, app_password)
 
-    print("OTP EMAIL SENT SUCCESSFULLY")
+        server.sendmail(
+            sender_email,
+            to_email,
+            msg.as_string()
+        )
+
+        server.quit()
+
+        print("MAIL SENT SUCCESSFULLY")
+
+    except Exception as e:
+
+        print("MAIL ERROR:", str(e))
+        raise e
