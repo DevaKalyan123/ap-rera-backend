b9827a09bf81fa5f293a54ffe9e61d0ef04d072b
DevaKalyan123 <devakalyaneepi@gmail.com>
2026-05-15 11:02:49 +0530

    fixed send_email_otp import

--- app/utils/mail_service.py
+++ app/utils/mail_service.py
@@ -48,8 +48,7 @@ def send_email(to_email, subject, body):
 # =========================================
 # OTP EMAIL
 # =========================================
-
-def send_otp_email(to_email, otp):
+def send_email_otp(to_email, otp):
 
     subject = "AP RERA OTP Verification"
 
